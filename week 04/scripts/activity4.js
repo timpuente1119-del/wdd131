90a665770f77de03f26b0777c9023fd463849591
timpuente1119-del <timpuente1119@gamil.com>
2026-09-25 10:20:51 -0700

    using map, filter, and reduce

---
+++ week 04/scripts/activity4.js
@@ -0,0 +1,12 @@
+let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
+
+let namesB = names.filter(name => name.charAt(0) === "B");
+
+console.log(namesB);
+
+let namesLength = names.map(name => name.length());
+
+
+
+let average = names.reduce((total, name) => total + name.length, 0) / names.length;
+console.log(average)
